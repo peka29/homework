fa678da3c3235f4d9a2c0d721cb38d6f232cfb38
Ekaterina <peka29@yandex.ru>
2023-03-08 23:10:30 +0300

    Дописано решение задачи 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -5,12 +5,17 @@
 // a = -3, b = -9 -> -3
 
 Console.Clear();
-Console.Write("Введите числа ");
+Console.Write("Введите число 1: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число ");
+Console.Write("Введите число 2: ");
 int b = Convert.ToInt32(Console.ReadLine());
 int max = 0;
+int min = 0;
 if (a > b) max = a;
 else
     max = b;
+if (a < b) min = a;
+else
+    min = b;
     Console.WriteLine($"Максимальное число {max}");
+    Console.WriteLine($"Минимальное число {min}");
